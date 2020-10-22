bc258234f221eab4f89d8e117877d882cb4bd84d
Murat Ersin <mmuratersin@gmail.com>
2020-10-22 22:07:19 +0300

    Error Page. This Fixes #17

--- app/middlewares/error-handler.js
+++ app/middlewares/error-handler.js
@@ -3,7 +3,8 @@ function errorHandler(err, req, res, next) {
   const status = err.status || 500;
 
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.errorTitle = status === 404 ? 'Not Found!' : 'Something went wrong';
+  res.locals.message = status === 404 ? err.message : 'You may also refresh the page or try again later';
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(status);
