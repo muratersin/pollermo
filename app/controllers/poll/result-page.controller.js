a27ca64348f3f63efd559c301d0dba8c5a13263b
Murat Ersin <mmuratersin@gmail.com>
2020-10-25 15:53:46 +0300

    BugFix: Percent

--- app/controllers/poll/result-page.controller.js
+++ app/controllers/poll/result-page.controller.js
@@ -4,7 +4,7 @@ function resultPageController(req, res) {
   poll.options = poll.options.map((o) => {
     const option = o;
     const totalVote = poll.totalVote || 1;
-    option.percent = Math.round((option.voteCount * 100) / totalVote);
+    option.percent = ((option.voteCount * 100) / totalVote).toFixed(2).replace('.00', '');
     return option;
   });
 
