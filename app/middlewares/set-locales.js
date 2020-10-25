b110d0e228435aeb0515bffa74bfd0a74f8279dc
Murat Ersin <mmuratersin@gmail.com>
2020-10-25 16:04:11 +0300

    Add Veersion Assets

--- app/middlewares/set-locales.js
+++ app/middlewares/set-locales.js
@@ -1,6 +1,9 @@
+const { version } = require('../../package.json');
+
 function setLocales(req, res, next) {
   res.locals.DOMAIN = process.env.DOMAIN;
   res.locals.CDN = process.env.CDN;
+  res.locals.VERSION = version;
   next();
 }
 
