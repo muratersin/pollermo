38e1079be6d7c44e8f2a3567005304385487eb65
Murat Ersin <mmuratersin@gmail.com>
2020-10-25 08:54:38 +0300

    Add Logger Closes #36

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const pollRouter = require('./routes/poll');
 
+const { accessLogger, errorLogger } = require('./utils/logger');
 const notFoundMiddleware = require('./middlewares/not-found');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 const setLocalesMiddleware = require('./middlewares/set-locales');
@@ -27,7 +27,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('trust proxy', true);
 
-app.use(logger('dev'));
+app.use(accessLogger);
+app.use(errorLogger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
