51c24339bf124e34b324de95a6968761be5a8687
Murat Ersin <mmuratersin@gmail.com>
2020-10-31 19:51:04 +0300

    BugFix

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -3,14 +3,10 @@ const fs = require('fs');
 
 const morgan = require('morgan');
 
-const requestLogFilePath = path.join(__dirname, '../../tmp/request.log');
 const errorLogFilePath = path.join(__dirname, '../../tmp/error.log');
 
 const requestLogger = morgan('combined', {
   skip: (req, res) => res.statusCode > 399,
-  stream: fs.createWriteStream(requestLogFilePath, {
-    flags: 'a',
-  }),
 });
 
 const errorLogger = morgan('combined', {
