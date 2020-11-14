7eea8f5aa30171af9260d8af9a960bd9edad9687
Murat Ersin <mmuratersin@gmail.com>
2020-11-14 16:38:14 +0300

    BugFixes

--- app/models/vote.js
+++ app/models/vote.js
@@ -2,23 +2,24 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const voteSchema = new Schema({
-  pollId: {
-    type: Schema.Types.ObjectId,
-    required: true,
+const voteSchema = new Schema(
+  {
+    pollId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    optionId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    ip: {
+      type: String,
+      required: true,
+    },
   },
-  optionId: {
-    type: Schema.Types.ObjectId,
-    required: true,
+  {
+    timestamps: true,
   },
-  ip: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-});
+);
 
 module.exports = mongoose.model('Vote', voteSchema);
